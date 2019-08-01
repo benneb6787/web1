cc09cf7ae68a85d99ed2a9e6393f9d53668c86b9
benneb6787 <benneb6786@gmail.com>
2019-08-01 16:44:57 +0900

    Update colors.js

--- javascript/colors.js
+++ javascript/colors.js
@@ -1,28 +1,32 @@
 var setcolor = {
   span:function(color){
-    var i = 0;
-    var slist = document.querySelectorAll('span');
-    while(i < slist.length) {
-      slist[i].style.background = color;
-      i++;
-    }
+    // var i = 0;
+    // var slist = document.querySelectorAll('span');
+    // while(i < slist.length) {
+    //   slist[i].style.background = color;
+    //   i++;
+    // }
+    $('span').css('background', color);
   },
   colb:function(color){
-    var i = 0;
-    var clist = document.querySelectorAll('.colb');
-    while(i < clist.length) {
-      clist[i].style.color = color;
-      i++;
-    }
+    // var i = 0;
+    // var clist = document.querySelectorAll('.colb');
+    // while(i < clist.length) {
+    //   clist[i].style.color = color;
+    //   i++;
+    // }
+    $('.colb').css('color', color);
   }
 }
 
 var body = {
   setcolor:function(color) {
-      document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   },
   setbackgroundcolor:function(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   }
 }
 
